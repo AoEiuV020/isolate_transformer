8625cc9162afc8f758c4d73a8a8069565ce344da
AoEiuV020 <aoeiuv020@gmail.com>
2024-04-18 12:22:52 +0800

    analyze

--- packages/isolate_transformer/test/isolate_transformer_test.dart
+++ packages/isolate_transformer/test/isolate_transformer_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:isolate_transformer/isolate_transformer.dart';
-
 void main() {
   test('adds one to input values', () {
     expect(1 + 1, 2);
