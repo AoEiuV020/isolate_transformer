d3939729d8a5b8e70e29e3489863373c782ef8fa
AoEiuV020 <aoeiuv020@gmail.com>
2024-04-21 18:03:46 +0800

    调整isolate内部判断done

--- packages/isolate_transformer/lib/async/isolate_transformer.dart
+++ packages/isolate_transformer/lib/async/isolate_transformer.dart
@@ -7,6 +7,30 @@ import 'package:isolate_transformer/isolate_transformer.dart';
 class IsolateTransformerImpl implements IsolateTransformer {
   final Set<Isolate> _cache = {};
 
+  /// 必须独立独立出来，
+  /// 否则mainReceive.close()会导致ReceivePort传进isolate中报错，有点奇怪，
+  void Function(SendPort) entry<S, T>(Stream<T> Function(Stream<S> e) mapper) =>
+      (SendPort sendPort) {
+        final receivePort = ReceivePort();
+        sendPort.send(receivePort.sendPort);
+        final streamController = StreamController<S>();
+        mapper(streamController.stream).listen((event) {
+          sendPort.send(event);
+        }, onDone: () {
+          receivePort.close();
+        }, onError: (e, s) {
+          if (e is Error || e is Exception) {
+            // 异常传到主线程再抛出，
+            sendPort.send(IsolateException(e, s));
+          }
+        });
+        receivePort.listen((event) {
+          streamController.add(event as S);
+        }, onDone: () {
+          streamController.close();
+        });
+      };
+
   @override
   Stream<T> transform<S, T>(
       Stream<S> data, Stream<T> Function(Stream<S> e) mapper,
@@ -15,50 +39,18 @@ class IsolateTransformerImpl implements IsolateTransformer {
       yield* mapper(data);
       return;
     }
-    var mainReceive = ReceivePort();
-    final isolate = await Isolate.spawn((SendPort sendPort) {
-      final receivePort = ReceivePort();
-      sendPort.send(receivePort.sendPort);
-      final streamController = StreamController<S>();
-      mapper(streamController.stream).listen((event) {
-        sendPort.send(event);
-      }, onDone: () {
-        // 这里把sendPort当成结束的标记使用，
-        sendPort.send(sendPort);
-      }, onError: (e, s) {
-        if (e is Error || e is Exception) {
-          // 异常传到主线程再抛出，
-          sendPort.send(IsolateException(e, s));
-        }
-      });
-      receivePort.listen((event) {
-        // 这里把sendPort当成结束的标记使用，
-        if (event == receivePort.sendPort) {
-          streamController.close();
-          return;
-        }
-        streamController.add(event as S);
-      }, onDone: () {
-        streamController.close();
-      });
-    }, mainReceive.sendPort,
+    final mainReceive = ReceivePort();
+    final isolate = await Isolate.spawn(entry(mapper), mainReceive.sendPort,
         // 这里注册监听异步线程内抛出的异常，
         onError: mainReceive.sendPort);
 
     _cache.add(isolate);
     await for (var message in mainReceive) {
-      if (message == mainReceive.sendPort) {
-        // 这里把sendPort当成结束的标记使用，
-        isolate.kill(priority: Isolate.immediate);
-        _cache.remove(isolate);
-        return;
-      }
       if (message is SendPort) {
         data.listen((event) {
           message.send(event);
         }, onDone: () {
-          // 这里把sendPort当成结束的标记使用，
-          message.send(message);
+          mainReceive.close();
         });
         continue;
       }
