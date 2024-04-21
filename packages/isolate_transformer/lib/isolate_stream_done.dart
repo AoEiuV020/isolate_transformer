b688bf74867b7dc9a1aab75597a90fa069429afa
AoEiuV020 <aoeiuv020@gmail.com>
2024-04-21 23:55:37 +0800

    fixup

--- packages/isolate_transformer/lib/isolate_stream_done.dart
+++ packages/isolate_transformer/lib/isolate_stream_done.dart
@@ -2,7 +2,7 @@
 ///
 /// 主要针对web端的Worker，
 /// 一方面传入的流无法关闭，
-/// 一方面异步worker内调用close外部也收不到结束，
+/// 一方面异步worker内调用close外部也收不到结束事件，
 class IsolateStreamDone {
   IsolateStreamDone();
   Map<String, String> toJson() => const {
