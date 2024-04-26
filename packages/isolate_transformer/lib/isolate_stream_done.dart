81f284b29b8da17a4543984975d3d259fecb2eee
AoEiuV020 <aoeiuv020@gmail.com>
2024-04-26 15:29:01 +0800

    补个注释

--- packages/isolate_transformer/lib/isolate_stream_done.dart
+++ packages/isolate_transformer/lib/isolate_stream_done.dart
@@ -1,7 +1,7 @@
 /// 用于标记Stream结束，
 ///
 /// 数据传进异步线程这一步是无阻塞的，所以数据源的读取可能过快，
-/// 为避免提前关闭了异步线程，需要把
+/// 为避免提前关闭了异步线程，需要把这个对象当成结束标志，数据发完了就发一个done对象，
 class IsolateStreamDone {
   const IsolateStreamDone();
   Map<String, String> toJson() => const {
